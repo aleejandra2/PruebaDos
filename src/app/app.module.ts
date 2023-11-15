b3417ede209c4f6939cebfc8519af9e1f6d9a55a
ZMarv <anto.rojas@duocuc.cl>
2023-11-14 23:57:10 -0300

    Revert "pruebas"
    This reverts commit b3c02e017955e3cb966bb42112e613473cb84b78.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy, isPlatform } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { platform } from 'os';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,20 +17,3 @@ import { platform } from 'os';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-const getConfig = () => {
-  if (isPlatform('hybrid')) {
-    return {
-      hardwareBackButton: true
-    };
-  }
-  return {};
-};
-
-const ionicConfig = {
-  ...getConfig(),
-  rippleEffect: false,
-  mode: 'md' as const
-};
-
-IonicModule.forRoot(ionicConfig);
